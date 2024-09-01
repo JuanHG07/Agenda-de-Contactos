86d08a533e7793c02cf3581524058c1fcbde99f5
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2024-09-01 14:52:30 -0500

    Add files via upload

---
+++ agenda/src/main/java/co/edu/uniquindio/poo/Data.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.poo;
+
+public class Data {
+    public static void main(String[] args) {
+        Agenda miagenda = new Agenda("Juan");
+        miagenda.menu();
+    }
+}
---
+++ agenda/src/main/java/co/edu/uniquindio/poo/Grupo.java
@@ -0,0 +1,52 @@
+package co.edu.uniquindio.poo;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Grupo {
+    public String nombre;
+    public String categoria;
+    public Collection<Contacto> contactosg;
+
+    public Grupo(String nombre, String categoria) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        contactosg = new LinkedList<>();
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Collection<Contacto> getContactosg() {
+        return contactosg;
+    }
+
+    public void setContactosg(Collection<Contacto> contactosg) {
+        this.contactosg = contactosg;
+    }
+
+    @Override
+    public String toString() {
+        String mensaje = "-Grupo-\nNombre: " + nombre + "\nCategoría: " + categoria + "\nLista de Contactos:\n";
+        int i = 1;
+        for (Contacto contacto : contactosg) {
+            mensaje += "- Contacto #" + i + ":\n" + contacto.toString() + "\n";
+            i++;
+        }
+        return mensaje;
+    }
+}
